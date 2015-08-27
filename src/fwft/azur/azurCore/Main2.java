7ea208e1dc421c48ebcdafd315154d7ca67a7dc5
ArnaudDebec <debec.arnaud@gmail.com>
2015-08-27 17:26:31 +0200

    .

--- src/fwft/azur/azurCore/Main2.java
+++ src/fwft/azur/azurCore/Main2.java
@@ -3,28 +3,17 @@ package fwft.azur.azurCore;
 public class Main2 {
 
 	public static void main(String[] args) {
-		Machine m1 = new Machine("m1", "127.0.0.1");
-		Machine m2 = new Machine("m2", "127.0.0.1");
+		Machine m = new Machine("m1", "127.0.0.1");
 		
-		RequestPort rp = new RequestPort(m1, m2);
+		RequestPort rp = new RequestPort(m, m);
 
 		Thread tSrp = new Thread(new ServerRequestPort());
-
-		Thread tCrp = new Thread(new ClientRequestPort(rp));
-		Thread tCrp2 = new Thread(new ClientRequestPort(rp));
-		
 		tSrp.start();
 		
-		tCrp.start();
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(int i=0; i<5; i++) {
+			Thread tCrp = new Thread(new ClientRequestPort(rp));
+			tCrp.start();			
 		}
-		
-		tCrp2.start();
 		
 	}
 
